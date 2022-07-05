5ab28b09fe780ea60db365759fb88b19b12c5eb5
SumanshVerma <77050180+SumanshVerma@users.noreply.github.com>
2022-07-05 10:53:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+    import {Desktop1} from "./Desktop1";
+
+    export default function App() {
+        document.body.style.backgroundColor = '#1e1e1e';
+        document.body.style.margin = '0';
+        return <Desktop1 />;
+    }
---
+++ src/Desktop1.js
@@ -0,0 +1,661 @@
+import React from "react";
+import styled from "styled-components";
+export const Desktop1 = ({}) => {
+  return (
+    <DesktopRoot>
+      <TimberwolfRectangle />
+      <Background>
+        <Vishnu>
+          <Image2 src={"https://file.rendit.io/n/ndiQpUdPEiUpjemRf6JW.png"} />
+          <FlexRow>
+            <Text1>CONNECT WALLET</Text1>
+          </FlexRow>
+        </Vishnu>
+        <Image3 src={"https://file.rendit.io/n/bwnWEQEOyZQfM1dO4wFC.svg"} />
+        <Element1>
+          <Text2>
+            CHRONICLE<Text3 fontWeight={"700"}> </Text3>
+            <Text3 fontWeight={"400"}>
+              whitelist
+              <br />
+              Now Open !<br />
+            </Text3>
+          </Text2>
+          <Image4 src={"https://file.rendit.io/n/cPw4c9BWbHASavvLwSHj.svg"} />
+          <Text5>
+            Shastra NFT
+            <Paragraph fontSize={"32px"}>
+              {" "}
+              is all you need to jump into our
+              <br />
+              metaverse , the only pass you’ll ever need to experience
+              “MetaAshram”. Join Our Discord
+              <br />
+              community now .
+            </Paragraph>
+          </Text5>
+        </Element1>
+        <Element2>
+          <FlexRow1>
+            <Text1>CONNECT WALLET</Text1>
+          </FlexRow1>
+          <Text7>
+            Shastra NFT
+            <Paragraph fontSize={"48px"}>
+              {" "}
+              Collection is{"\n"}designed keeping India in mind,{"\n"}with 15
+              hand-painted digital{"\n"}artworks that showcase the{"\n"}artistry
+              of Indian artists.{" "}
+            </Paragraph>
+          </Text7>
+          <Paragraph2>
+            In the near future, We will have a metaverse. This means that if you
+            own an a <Text8>Shastra NFT</Text8>
+            <Paragraph fontSize={"48px"}>
+              , you will be able to access it in the game and earn more NFTs.
+            </Paragraph>
+          </Paragraph2>
+          <SBGChShartsm
+            src={"https://file.rendit.io/n/xqGMuNrw1KRCSkDMDNs0.png"}
+          />
+          <Image1 src={"https://file.rendit.io/n/dDyPzjmNEGabHc4C3t7O.png"} />
+          <COLLECTGETACCESS>
+            COLLECT & <br />
+            GET ACCESS
+          </COLLECTGETACCESS>
+          <METAASHRAMACCESS>
+            META-ASHRAM
+            <br />
+            ACCESS
+          </METAASHRAMACCESS>
+          <Text9>NFT DROPS</Text9>
+          <Arrow src={"https://file.rendit.io/n/kQ2PXziYAdzDjqpkeOSg.svg"} />
+          <Arrow1 src={"https://file.rendit.io/n/kQ2PXziYAdzDjqpkeOSg.svg"} />
+          <Arrow2 src={"https://file.rendit.io/n/QjyDmG2xyVNePFglrglj.svg"} />
+          <Image5 src={"https://file.rendit.io/n/vpGjB5fzbbIYcBgCu5fs.svg"} />
+        </Element2>
+        <FlexRow2>
+          <S1>
+            <Image6
+              src={"https://file.rendit.io/n/4thTYCzEqXLP0UKnaBvK.svg"}
+              left={"12.75px"}
+            />
+            <Element3 />
+          </S1>
+          <SBGChShartsm1
+            src={"https://file.rendit.io/n/N9CAC8PnWn0hMlJdzz3r.png"}
+          />
+          <SBGChShart
+            src={"https://file.rendit.io/n/TN3vszVVMcBu7ZjC4H2y.png"}
+          />
+          <SBGChShartsm2
+            src={"https://file.rendit.io/n/zF7wpCKYdcHt0RBrNLct.png"}
+          />
+          <S>
+            <Image6
+              src={"https://file.rendit.io/n/EJmHfUeotDvSoEoKfzUL.svg"}
+              left={"14px"}
+            />
+            <Element3 />
+          </S>
+        </FlexRow2>
+        <FlexRow3>
+          <FlexColumn margin={"0px 35px 0px 0px"}>
+            <Text10>Arjun's enlightment</Text10>
+            <FlexRow4 alignItems={"center"}>
+              <Ellipse
+                src={"https://file.rendit.io/n/UnIapIbmW6yiG3Oou2pb.png"}
+              />
+              <Text11>@KlipVr</Text11>
+            </FlexRow4>
+          </FlexColumn>
+          <Text12 margin={"6px 136px 0px 0px"}>21,5K Likes</Text12>
+          <FlexColumn1>
+            <Text10>Art of Concentration</Text10>
+            <FlexRow4 alignItems={"center"}>
+              <Ellipse
+                src={"https://file.rendit.io/n/UnIapIbmW6yiG3Oou2pb.png"}
+              />
+              <Text11>@KlipVr</Text11>
+            </FlexRow4>
+          </FlexColumn1>
+          <Text12 margin={"4px 142px 0px 0px"}>21,5K Likes</Text12>
+          <FlexColumn margin={"0px 66px 0px 0px"}>
+            <Text10>The Time Within </Text10>
+            <FlexRow4 alignItems={"center"}>
+              <Ellipse
+                src={"https://file.rendit.io/n/UnIapIbmW6yiG3Oou2pb.png"}
+              />
+              <Text11>@KlipVR</Text11>
+            </FlexRow4>
+          </FlexColumn>
+          <Text12 margin={"6px 0px 0px 0px"}>21,5K Likes</Text12>
+        </FlexRow3>
+        <FlexRow7>
+          <Text18 alignSelf={"inherit"}>Price</Text18>
+          <FlexRow8>
+            <Ethdiamondpurple
+              src={"https://file.rendit.io/n/lB0Mgrkvqg6bxMSgPPMR.png"}
+            />
+            <ETH2>
+              1.10<ETH> </ETH>
+              <ETH1>ETH</ETH1>
+            </ETH2>
+          </FlexRow8>
+          <Text18 alignSelf={"flex-start"}>Price</Text18>
+          <FlexRow9>
+            <Ethdiamondpurple
+              src={"https://file.rendit.io/n/lB0Mgrkvqg6bxMSgPPMR.png"}
+            />
+            <ETH2>
+              1.10<ETH> </ETH>
+              <ETH1>ETH</ETH1>
+            </ETH2>
+          </FlexRow9>
+          <Text20>Price</Text20>
+          <FlexRow4 alignItems={"flex-start"}>
+            <Ethdiamondpurple
+              src={"https://file.rendit.io/n/lB0Mgrkvqg6bxMSgPPMR.png"}
+            />
+            <ETH2>
+              1.10<ETH> </ETH>
+              <ETH1>ETH</ETH1>
+            </ETH2>
+          </FlexRow4>
+        </FlexRow7>
+        <Component>
+          <Element5>
+            <Iconfacebook
+              src={"https://file.rendit.io/n/rZjcC5fBNeSQeo9kzMhr.svg"}
+            />
+            <Iconinstagram
+              src={"https://file.rendit.io/n/LpnAoXdEIBI5tMXenuGj.svg"}
+            />
+          </Element5>
+          <Iconlinkedin
+            src={"https://file.rendit.io/n/WTTWWVybdN7uovDpHyg0.svg"}
+          />
+        </Component>
+        <WhiteParagraph>
+          © 2022 All Rights Reserved by Klip VR Immersive Technologies
+        </WhiteParagraph>
+      </Background>
+      <Image8 src={""} />
+    </DesktopRoot>
+  );
+};
+const DesktopRoot = styled.div`
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  height: 3579px;
+  background-color: #ffffff;
+  overflow: hidden;
+  position: relative;
+  margin: auto;
+  min-width: 1440px;
+`;
+const TimberwolfRectangle = styled.div`
+  width: 1440px;
+  height: 2573px;
+  background-color: #d9d9d9;
+  position: absolute;
+`;
+const Background = styled.div`
+  height: 3579px;
+  background-image: url("https://file.rendit.io/n/ZvcZdv4ZtxEPmlizQZxt.png");
+  background-size: cover;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Vishnu = styled.div`
+  width: 1422px;
+  height: 79.6px;
+  background-image: url("https://file.rendit.io/n/GzNxn40i7n7XrmFd8JN9.png");
+  background-size: cover;
+  align-self: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 905px;
+  align-items: center;
+  padding: 23px 9px 921.4px 9px;
+  margin: 0px 0px 34.31px 0px;
+`;
+const Image2 = styled.img`
+  width: 264px;
+  height: 79.6px;
+`;
+const FlexRow = styled.div`
+  border-width: 1px;
+  border-color: #ffffff;
+  border-style: solid;
+  backdrop-filter: blur(4px);
+  width: 214px;
+  background-image: linear-gradient(
+    94deg,
+    rgba(83, 46, 98, 0.5) 0%,
+    rgba(83, 46, 98, 0) 134%
+  );
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-radius: 95px;
+  padding: 17px 0px;
+`;
+const Image3 = styled.img`
+  width: 142.91px;
+  height: 92.65px;
+  align-self: center;
+  margin: 0px 0px 24.04px 0px;
+`;
+const Element1 = styled.div`
+  height: 172px;
+  position: relative;
+  min-width: 1440px;
+  margin: 0px 0px 75px 0px;
+`;
+const Text2 = styled.div`
+  font-size: 60px;
+  font-family: League Spartan;
+  font-weight: 700;
+  color: #feb700;
+  position: absolute;
+  top: 7px;
+  left: 66px;
+`;
+const Image4 = styled.img`
+  width: 2px;
+  height: 141.5px;
+  position: absolute;
+  left: 721px;
+`;
+const Text5 = styled.div`
+  height: 117px;
+  font-size: 32px;
+  font-family: League Spartan;
+  font-weight: 500;
+  color: #ffffff;
+  position: absolute;
+  top: 7px;
+  left: 778px;
+`;
+const Element2 = styled.div`
+  height: 1252.16px;
+  position: relative;
+  min-width: 1440px;
+  margin: 0px 0px 17.84px 0px;
+`;
+const FlexRow1 = styled.div`
+  border-width: 1px;
+  border-color: #ffffff;
+  border-style: solid;
+  backdrop-filter: blur(4px);
+  width: 214px;
+  background-image: linear-gradient(
+    94deg,
+    rgba(83, 46, 98, 0.5) 0%,
+    rgba(83, 46, 98, 0) 134%
+  );
+  display: flex;
+  position: absolute;
+  top: 105px;
+  left: 1176px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-radius: 95px;
+  padding: 17px 0px;
+`;
+const Text7 = styled.div`
+  height: 192px;
+  font-size: 48px;
+  font-family: League Spartan;
+  font-weight: 600;
+  color: #feb700;
+  position: absolute;
+  top: 146px;
+  left: 55px;
+`;
+const Paragraph2 = styled.div`
+  height: 303px;
+  font-size: 48px;
+  font-family: League Spartan;
+  font-weight: 400;
+  color: #ffffff;
+  position: absolute;
+  top: 666px;
+  left: 774px;
+  white-space: pre-wrap;
+`;
+const Text8 = styled.div`
+  font-size: 48px;
+  font-family: League Spartan;
+  font-weight: 600;
+  color: #feb700;
+  display: contents;
+`;
+const SBGChShartsm = styled.img`
+  width: 653px;
+  height: 475px;
+  position: absolute;
+  left: 737px;
+`;
+const Image1 = styled.img`
+  width: 653px;
+  height: 475px;
+  position: absolute;
+  top: 532px;
+  left: 53px;
+`;
+const COLLECTGETACCESS = styled.div`
+  text-align: right;
+  width: 637px;
+  font-size: 48px;
+  font-family: Manrope;
+  color: #ffffff;
+  position: absolute;
+  top: 403px;
+  left: 704px;
+`;
+const METAASHRAMACCESS = styled.div`
+  width: 637px;
+  font-size: 48px;
+  font-family: Manrope;
+  color: #ffffff;
+  position: absolute;
+  top: 944px;
+  left: 122px;
+`;
+const Text9 = styled.div`
+  font-size: 60px;
+  font-family: League Spartan;
+  font-weight: 500;
+  color: #ffffff;
+  position: absolute;
+  top: 1130px;
+  left: 1012px;
+`;
+const Arrow = styled.img`
+  width: 188.32px;
+  height: 188.32px;
+  position: absolute;
+  top: 1063.84px;
+  left: 1234.84px;
+`;
+const Arrow1 = styled.img`
+  width: 188.32px;
+  height: 188.32px;
+  position: absolute;
+  top: 961.84px;
+  left: 258.84px;
+`;
+const Arrow2 = styled.img`
+  width: 30.87px;
+  height: 30.87px;
+  position: absolute;
+  top: 499.56px;
+  left: 1348.56px;
+`;
+const Image5 = styled.img`
+  width: 891.5px;
+  height: 2px;
+  position: absolute;
+  top: 1171px;
+  left: 85px;
+`;
+const FlexRow2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 46px;
+  margin: 0px 0px 46px 0px;
+`;
+const S1 = styled.div`
+  width: 52.1px;
+  height: 55px;
+  position: relative;
+  margin: 0px 25.9px 0px 0px;
+`;
+const SBGChShartsm1 = styled.img`
+  width: 316px;
+  height: 429px;
+  align-self: flex-end;
+  margin: 0px 112px 7px 0px;
+`;
+const SBGChShart = styled.img`
+  width: 316px;
+  height: 444px;
+  margin: 0px 112px 0px 0px;
+`;
+const SBGChShartsm2 = styled.img`
+  width: 316px;
+  height: 422px;
+  margin: 0px 26px 0px 0px;
+`;
+const S = styled.div`
+  width: 52.1px;
+  height: 55px;
+  position: relative;
+`;
+const FlexRow3 = styled.div`
+  height: 68px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px 110px 0px 152px;
+  margin: 0px 0px 19px 0px;
+`;
+const FlexColumn1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin: 0px 22px 0px 0px;
+`;
+const FlexRow7 = styled.div`
+  height: 23px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  padding: 0px 126px 0px 152px;
+  margin: 0px 0px 168px 0px;
+`;
+const FlexRow8 = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin: 0px 138px 0px 0px;
+`;
+const FlexRow9 = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-self: flex-start;
+  margin: 0px 144px 0px 0px;
+`;
+const Text20 = styled.div`
+  font-size: 16px;
+  font-family: DM Sans;
+  font-weight: 700;
+  color: #ffffff;
+  margin: 0px 166px 0px 0px;
+`;
+const Component = styled.div`
+  width: 72px;
+  display: flex;
+  align-self: center;
+  flex-direction: row;
+  justify-content: flex-end;
+  gap: 11px;
+  align-items: center;
+  margin: 0px 0px 48px 0px;
+`;
+const Element5 = styled.div`
+  width: 22px;
+  height: 21px;
+  position: relative;
+  flex-grow: 1;
+`;
+const Iconfacebook = styled.img`
+  width: 21px;
+  height: 21px;
+  position: absolute;
+  left: -34px;
+`;
+const Iconinstagram = styled.img`
+  width: 22px;
+  height: 21px;
+  position: absolute;
+`;
+const Iconlinkedin = styled.img`
+  width: 21px;
+  height: 21px;
+`;
+const WhiteParagraph = styled.div`
+  text-align: center;
+  display: flex;
+  font-size: 14px;
+  font-family: League Spartan;
+  font-weight: 300;
+  align-self: center;
+  backdrop-filter: blur(0px);
+  width: 1440px;
+  height: 13px;
+  background-color: rgba(255, 255, 255, 0.35);
+  flex-direction: row;
+  justify-content: center;
+  padding: 14px 0px 15px 0px;
+`;
+const Image8 = styled.img`
+  width: 351px;
+  height: 64px;
+  mix-blend-mode: color-burn;
+  position: absolute;
+  top: 471px;
+  left: -9856px;
+`;
+const Text1 = styled.div`
+  text-align: center;
+  width: 170px;
+  height: 16px;
+  font-size: 20px;
+  font-family: League Spartan;
+  font-weight: 400;
+  color: #ffffff;
+`;
+const Text3 = styled.div`
+  font-size: 60px;
+  font-family: League Spartan;
+  color: #ffffff;
+  display: contents;
+  white-space: pre-wrap;
+  font-weight: ${(props) => props.fontWeight};
+`;
+const Paragraph = styled.div`
+  font-family: League Spartan;
+  font-weight: 400;
+  color: #ffffff;
+  display: contents;
+  white-space: pre-wrap;
+  font-size: ${(props) => props.fontSize};
+`;
+const Image6 = styled.img`
+  width: 24.75px;
+  height: 22.09px;
+  position: absolute;
+  top: 16.45px;
+  left: ${(props) => props.left};
+`;
+const Element3 = styled.div`
+  border-width: 3px;
+  border-color: #ffffff;
+  border-style: solid;
+  width: 49.1px;
+  height: 52px;
+  position: absolute;
+  top: -1px;
+  left: -1px;
+  border-radius: 70px;
+`;
+const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-self: flex-end;
+  margin: ${(props) => props.margin};
+`;
+const Text10 = styled.div`
+  font-size: 20px;
+  font-family: DM Sans;
+  font-weight: 700;
+  color: #ffffff;
+`;
+const FlexRow4 = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: ${(props) => props.alignItems};
+`;
+const Ellipse = styled.img`
+  width: 28px;
+  height: 28px;
+`;
+const Text11 = styled.div`
+  font-size: 16px;
+  font-family: DM Sans;
+  font-weight: 400;
+  color: #ffffff;
+`;
+const Text12 = styled.div`
+  text-align: right;
+  font-size: 16px;
+  font-family: DM Sans;
+  font-weight: 400;
+  color: #ffffff;
+  margin: ${(props) => props.margin};
+`;
+const Text18 = styled.div`
+  font-size: 16px;
+  font-family: DM Sans;
+  font-weight: 700;
+  color: #ffffff;
+  margin: 0px 180px 0px 0px;
+  align-self: ${(props) => props.alignSelf};
+`;
+const Ethdiamondpurple = styled.img`
+  width: 11px;
+  height: 18px;
+`;
+const ETH2 = styled.div`
+  text-align: right;
+  font-size: 16px;
+  font-family: DM Sans;
+  font-weight: 700;
+  color: #fcddec;
+`;
+const ETH = styled.div`
+  font-size: 16px;
+  font-family: DM Sans;
+  font-weight: 700;
+  color: #747474;
+  display: contents;
+  white-space: pre-wrap;
+`;
+const ETH1 = styled.div`
+  font-size: 16px;
+  font-family: DM Sans;
+  font-weight: 400;
+  color: #ffffff;
+  display: contents;
+`;
